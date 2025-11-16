2228d5c4c14d741c463ada24e683e02a8dd2e754
snowsphere <info@snowsphere.net>
2025-11-16 11:22:48 +0900

    fix: スペースに変更

--- src/index.ts
+++ src/index.ts
@@ -21,103 +21,115 @@ import type { MisskeyPage } from './types/Misskey'
 const KV_KEY = 'lastPostedText'
 
 const randomString = (n = 10) => {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const array = new Uint8Array(n);
-  crypto.getRandomValues(array);
-  return Array.from(array, v => chars[v % chars.length]).join('');
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const array = new Uint8Array(n)
+  crypto.getRandomValues(array)
+  return Array.from(array, (v) => chars[v % chars.length]).join('')
 }
 
 export default {
-	async fetch(req) {
-		const url = new URL(req.url);
-		url.pathname = '/__scheduled';
-		url.searchParams.append('cron', '* * * * *');
-		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
-	},
+  async fetch(req) {
+    const url = new URL(req.url)
+    url.pathname = '/__scheduled'
+    url.searchParams.append('cron', '* * * * *')
+    return new Response(
+      `To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`
+    )
+  },
 
-	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
-	// [[triggers]] configuration.
-	async scheduled(_event, env, _ctx): Promise<void> {
-		const playingStatusRes = await fetch(`https://api.listenbrainz.org/1/user/${env.LISTEN_BRAINZ_USER_NAME}/playing-now`)
+  // The scheduled handler is invoked at the interval set in our wrangler.jsonc's
+  // [[triggers]] configuration.
+  async scheduled(_event, env, _ctx): Promise<void> {
+    const playingStatusRes = await fetch(
+      `https://api.listenbrainz.org/1/user/${env.LISTEN_BRAINZ_USER_NAME}/playing-now`
+    )
 
-		if (!playingStatusRes.ok) {
-			console.error(`cannot fetch playing status: ${playingStatusRes.status}`)
-			return;
-		}
+    if (!playingStatusRes.ok) {
+      console.error(`cannot fetch playing status: ${playingStatusRes.status}`)
+      return
+    }
 
-		const playingStatus = await playingStatusRes.json() as ListenBrainsResponse
-		console.log(JSON.stringify(playingStatus))
+    const playingStatus =
+      (await playingStatusRes.json()) as ListenBrainsResponse
+    console.log(JSON.stringify(playingStatus))
 
-		const metadata = playingStatus.payload.listens.length > 0 ? playingStatus.payload.listens[0].track_metadata : null
-		let postText = metadata
-			? `${metadata.artist_name} - ${metadata.track_name} ${ metadata.release_name
-				? `(from: ${metadata.release_name})`
-				: '' }`
-			: 'nothing playing!'
+    const metadata =
+      playingStatus.payload.listens.length > 0
+        ? playingStatus.payload.listens[0].track_metadata
+        : null
+    let postText = metadata
+      ? `${metadata.artist_name} - ${metadata.track_name} ${
+          metadata.release_name ? `(from: ${metadata.release_name})` : ''
+        }`
+      : 'nothing playing!'
 
-		if (await env.nowplaying_gorilla.get(KV_KEY) === postText) {
-			console.log('nothing update. exit.')
-			return;
-		}
+    if ((await env.nowplaying_gorilla.get(KV_KEY)) === postText) {
+      console.log('nothing update. exit.')
+      return
+    }
 
-		env.nowplaying_gorilla.put(KV_KEY, postText)
+    env.nowplaying_gorilla.put(KV_KEY, postText)
 
-		if (metadata?.additional_info.origin_url)
-			postText += `\n${metadata.additional_info.origin_url}`
+    if (metadata?.additional_info.origin_url)
+      postText += `\n${metadata.additional_info.origin_url}`
 
-		const pageRes = await fetch (`https://${env.MISSKEY_HOST}/api/pages/show`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				pageId: env.MISSKEY_PAGE_ID
-			})
-		})
+    const pageRes = await fetch(`https://${env.MISSKEY_HOST}/api/pages/show`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        pageId: env.MISSKEY_PAGE_ID
+      })
+    })
 
-		if (!pageRes.ok) {
-			console.error(`cannot fetch page!: ${JSON.stringify(await pageRes.json())}`)
-			return
-		}
+    if (!pageRes.ok) {
+      console.error(
+        `cannot fetch page!: ${JSON.stringify(await pageRes.json())}`
+      )
+      return
+    }
 
-		const pageData = await pageRes.json() as MisskeyPage
+    const pageData = (await pageRes.json()) as MisskeyPage
 
-		const payload = {
-			pageId: env.MISSKEY_PAGE_ID,
-			title: pageData.title,
-			summary: pageData.summary,
-			name: pageData.name,
-			eyeCatchingImageId: pageData.eyeCatchingImageId,
-			alignCenter: false,
-			hideTitleWhenPinned: false,
-			font: pageData.font,
-			script: pageData.script,
-			variables: [],
-			content: [
-				{
-					id: randomString(),
-					type: 'text',
-					text: postText
-				}
-			]
-		}
+    const payload = {
+      pageId: env.MISSKEY_PAGE_ID,
+      title: pageData.title,
+      summary: pageData.summary,
+      name: pageData.name,
+      eyeCatchingImageId: pageData.eyeCatchingImageId,
+      alignCenter: false,
+      hideTitleWhenPinned: false,
+      font: pageData.font,
+      script: pageData.script,
+      variables: [],
+      content: [
+        {
+          id: randomString(),
+          type: 'text',
+          text: postText
+        }
+      ]
+    }
 
-		try {
-			const postRes = await fetch(`https://${env.MISSKEY_HOST}/api/pages/update`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${env.MISSKEY_TOKEN}`
-				},
-				body: JSON.stringify(payload)
-			})
+    try {
+      const postRes = await fetch(
+        `https://${env.MISSKEY_HOST}/api/pages/update`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${env.MISSKEY_TOKEN}`
+          },
+          body: JSON.stringify(payload)
+        }
+      )
 
-			if (!postRes.ok) {
-				console.error(await postRes.json())
-			}
-		} catch (err) {
-			console.log(err)
-		}
-	},
-
-} satisfies ExportedHandler<Env>;
+      if (!postRes.ok) {
+        console.error(await postRes.json())
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+} satisfies ExportedHandler<Env>
--- src/types/ListenBrains.ts
+++ src/types/ListenBrains.ts
@@ -1,30 +1,30 @@
 export interface ListenBrainsResponse {
-  payload: Payload;
+  payload: Payload
 }
 
 interface Payload {
-  count: number;
-  listens: Listen[];
-  playing_now: boolean;
-  user_id: string;
+  count: number
+  listens: Listen[]
+  playing_now: boolean
+  user_id: string
 }
 
 interface Listen {
-  playing_now: boolean;
-  track_metadata: Trackmetadata;
+  playing_now: boolean
+  track_metadata: Trackmetadata
 }
 
 interface Trackmetadata {
-  additional_info: Additionalinfo;
-  artist_name: string;
-  release_name: string;
-  track_name: string;
+  additional_info: Additionalinfo
+  artist_name: string
+  release_name: string
+  track_name: string
 }
 
 interface Additionalinfo {
-	music_service_name?: string,
-	origin_url?: string
-  duration_ms: number;
-  submission_client: string;
-  submission_client_version: string;
+  music_service_name?: string
+  origin_url?: string
+  duration_ms: number
+  submission_client: string
+  submission_client_version: string
 }
